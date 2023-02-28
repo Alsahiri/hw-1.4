0eb93a1406adddbbb8b2ac0fe4ff3e806ec04fc0
Alsahiri <sudarkin.jan@yandex.ru>
2023-02-28 20:53:48 +0600

    Homework done

--- src/Main.java
+++ src/Main.java
@@ -41,19 +41,63 @@ public class Main {
     }
     public static void task4 () {
     System.out.println("Задача 4");
-
-
+        int years = 2;
+        if (years >= 2 && years <= 6) {
+            System.out.println("Если возраст человека равен " + years + " то ему нужно ходить в детский сад");
+        } else if (years > 6 && years <= 18) {
+            System.out.println("Если возраст человека равен " + years + " то ему нужно ходить в школу");
+        } else if (years > 18 && years <= 24) {
+            System.out.println("Если возраст человека равен " + years + " то ему нужно ходить в университет");
+        } else {
+            System.out.println("Если возраст человека равен " + years + " то ему нужно ходить на работу");
+        }
     }
     public static void task5 () {
         System.out.println("Задача 5");
+        int age = 1;
+        if (age < 5) {
+            System.out.println("Если возраст ребенка равен "+age+" , то ему нельзя кататься на аттракционе");
+        } else if (age >= 5 && age < 14) {
+            System.out.println("Если возраст ребенка равен " + age + " , то ему можно кататься " +
+                    "на аттракционе в сопровождении взрослого");
+        } else {
+            System.out.println("Если возраст ребенка равен " + age + " , то ему можно кататься " +
+                    "на аттракционе без сопровождения взрослого");
+        }
 
     }
     public static void task6 () {
         System.out.println("Задача 6");
+        int capacity = 102;
+        int seatSeats = 60;
 
+        int people = 70;
+        if (people == capacity) {
+            System.out.println("Мест в вагоне нет");
+        } else if (people > seatSeats) {
+            System.out.println("Сидячие места заняты, но есть стоячие");
+        } else {
+            System.out.println("Сидячие места есть");
+        }
     }
     public static void task7 () {
         System.out.println("Задача 7");
+        int one = 101;
+        int two = 101;
+        int three = 101;
+        if (three >= one && three >= two) {
+            System.out.println(three);
+        } else if (three >= one && three <= two) {
+            System.out.println(two);
+        } else if (three <= one && three >= two) {
+            System.out.println(one);
+        } else if (three <= one && three <= two) {
+            if (one >= two) {
+                System.out.println(one);
+            } else {
+                    System.out.println(two);
+                }
+            }
+        }
 
     }
-}
